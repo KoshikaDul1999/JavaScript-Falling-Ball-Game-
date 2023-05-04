c751fe48147a55ec6b8a02efae07899fa865a7c8
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-04 17:37:06 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var character = document.getElementById("character");
+var interval;
 
 function moveLeft(){
     var left = 
@@ -11,3 +12,16 @@ function moveRight(){
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     character.style.left = left - 2 + "px";
 }
+
+document.addEventListener("keydown", event => {
+    if(event.key==="ArrowLeft"){
+        interval = setInterval(moveLeft, 1);
+    }
+    if(event.key==="ArrowRight"){
+        interval = setInterval(moveRight, 1);
+    }
+});
+
+document.addEventListener("keyup", event =>{
+    clearInterval(interval);
+});
