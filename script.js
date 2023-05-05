13a7e77c98264e46407c6f23adce3f4123b3e625
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-05 08:31:51 +0530

    updated

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 var character = document.getElementById("character");
+var game = document.getElementById("game");
 var interval;
+var both = 0; 
 
 function moveLeft(){
     var left = 
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character.style.left = left + 2 + "px";
+    if(left>0){
+        character.style.left = left + 2 + "px";
+    }  
 }
 
 function moveRight(){
     var left = 
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    character.style.left = left - 2 + "px";
+    if(left<380){
+        character.style.left = left - 2 + "px";
+    }
 }
 
 document.addEventListener("keydown", event => {
-    if(event.key==="ArrowLeft"){
-        interval = setInterval(moveLeft, 1);
-    }
-    if(event.key==="ArrowRight"){
-        interval = setInterval(moveRight, 1);
+    if(both==0){
+        both++;
+        if(event.key==="ArrowLeft"){
+            interval = setInterval(moveLeft, 1);
+        }
+        if(event.key==="ArrowRight"){
+            interval = setInterval(moveRight, 1);
+        }
     }
 });
 
 document.addEventListener("keyup", event =>{
     clearInterval(interval);
+    both = 0;
 });
+
+var block = document.createElement("div");
+var hole = document.createElement("div");
+block.setAttribute("class","block");
+hole.setAttribute("class","hole");
+block.setAttribute("id","block");
+hole.setAttribute("id","hole");
+game.appendChild(block);
+game.appendChild(hole);
